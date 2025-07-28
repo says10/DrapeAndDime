fa6cf0515fe4fc5c4b6463d861a448aaaa43134a
says10 <sayantanmukherjee0010@gmail.com>
2025-07-28 18:18:27 +0530

    before seo google index please take this because had lot of errors checkpoint

--- borcella_store-master/app/layout.tsx
+++ borcella_store-master/app/layout.tsx
@@ -17,9 +17,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: any;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
